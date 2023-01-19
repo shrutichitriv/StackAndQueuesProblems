dee90ea7346dd5b6fa03c20e003ccc8864acb986
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-19 06:50:54 +0530

    [Shruti] Stacks and queues program

---
+++ StackAndQueues_244/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackAndQueues_244
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Data Structure Program");
+
+        }
+    }
+}
